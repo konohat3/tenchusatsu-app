c32339f0f0ccd6226e23cf1dbb0b3cea9bf19d26
konohat3 <konohat3@gmail.com>
2026-01-19 17:48:21 +0900

    Update server.js

--- server.js
+++ server.js
@@ -321,4 +321,5 @@ app.post("/result", (req, res) => {
 });
 
 const PORT = process.env.PORT || 3000;
-app.listen(PORT, () => console.log(\`Listening on \${PORT}\`));
+app.listen(PORT, () => console.log("Listening on " + PORT));
+
